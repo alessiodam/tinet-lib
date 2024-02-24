bc9d52dfe4cd746ec4de5070e7640d60840f155e
TKB Studios <69647028+tkbstudios@users.noreply.github.com>
2024-02-24 14:25:37 +0100

    start refactoring for lwip

--- tinet.c
+++ tinet.c
@@ -26,80 +26,20 @@
  * please increase this counter as
  * a warning for the next person:
  *
- * Total hours wasted here = 16 hours.
+ * Total hours wasted here = 17 hours.
 */
 
 #include "tinet-lib/tinet.h"
 #include <string.h>
 #include <fileioc.h>
-#include <srldrvce.h>
 #include <time.h>
-#include <ti/info.h>
 
-// AUTH VARIABLES
-char *username;
-char *authkey;
 uint8_t NetKeyAppVar;
-
-// SERIAL VARIABLES
-srl_device_t srl_device;
-uint8_t srl_buf[512];
-bool has_srl_device = false;
-bool bridge_connected = false;
-bool tcp_connected = false;
-char tinet_net_buffer[2048];
-
-const system_info_t *systemInfo;
-
-static usb_error_t handle_usb_event(const usb_event_t event, void *event_data, usb_callback_data_t *callback_data __attribute__((unused))) {
-    usb_error_t err;
-
-    if ((err = srl_UsbEventCallback(event, event_data, callback_data)) != USB_SUCCESS)
-        return err;
-
-    if(event == USB_DEVICE_CONNECTED_EVENT && !(usb_GetRole() & USB_ROLE_DEVICE)) {
-        const usb_device_t device = event_data;
-        usb_ResetDevice(device);
-    }
-
-    if(event == USB_HOST_CONFIGURE_EVENT || (event == USB_DEVICE_ENABLED_EVENT && !(usb_GetRole() & USB_ROLE_DEVICE))) {
-        if(has_srl_device) return USB_SUCCESS;
-
-        usb_device_t device;
-        if(event == USB_HOST_CONFIGURE_EVENT) {
-            device = usb_FindDevice(NULL, NULL, USB_SKIP_HUBS);
-            if(device == NULL) return USB_SUCCESS;
-        } else {
-            device = event_data;
-        }
-
-        const srl_error_t error = srl_Open(&srl_device, device, srl_buf, sizeof srl_buf, SRL_INTERFACE_ANY, 115200);
-        if(error) {
-            return USB_SUCCESS;
-        }
-
-        has_srl_device = true;
-    }
-
-    if(event == USB_DEVICE_DISCONNECTED_EVENT) {
-        const usb_device_t device = event_data;
-        if(device == srl_device.dev) {
-            srl_Close(&srl_device);
-            has_srl_device = false;
-        }
-    }
-
-    return USB_SUCCESS;
-}
+char *username;
+char *calckey;
+char *sessiontoken;
 
 int tinet_init() {
-    const system_info_t *systemInfo = os_GetSystemInfo();
-
-    if (systemInfo->hardwareType2 != 9)
-    {
-        return TINET_UNSUPPORTED_CALC;
-    }
-
     NetKeyAppVar = ti_Open("NETKEY", "r");
     if (NetKeyAppVar == 0) {
         return TINET_NO_KEYFILE;
@@ -109,14 +49,8 @@ int tinet_init() {
     ti_Close(NetKeyAppVar);
 
     username = (char *)data_ptr;
-    authkey = (char *)(data_ptr + strlen(username) + 1);
+    calckey = (char *)(data_ptr + strlen(username) + 1);
 
-    const usb_error_t usb_error = usb_Init(handle_usb_event, NULL, srl_GetCDCStandardDescriptors(), USB_DEFAULT_INIT_FLAGS);
-
-    if(usb_error) {
-        usb_Cleanup();
-        return TINET_SRL_INIT_FAIL;
-    }
     return TINET_SUCCESS;
 }
 
@@ -124,157 +58,25 @@ char* tinet_get_username() {
     return username;
 }
 
-int tinet_connect(const int timeout) {
-    time_t start_time;
-    time_t current_time;
-    time(&start_time);
-
-    do {
-        time(&current_time);
-        if ((int)difftime(current_time, start_time) > timeout) {
-            return TINET_TIMEOUT_EXCEEDED;
-        }
-
-        const TINET_ReturnCode read_result = tinet_read_srl(tinet_net_buffer);
-        if (read_result > 0) {
-            if (strcmp(tinet_net_buffer, "BRIDGE_CONNECTED\n") == 0) {
-                bridge_connected = true;
-
-                const TINET_ReturnCode write_response = tinet_write_srl("CONNECT_TCP\n");
-                if (write_response == TINET_SUCCESS) {
-                    do {
-                        time(&current_time);
-                        if ((int)difftime(current_time, start_time) > timeout) {
-                            return TINET_TIMEOUT_EXCEEDED;
-                        }
-                        const int connect_tcp_read_bytes = tinet_read_srl(tinet_net_buffer);
-                        if (connect_tcp_read_bytes > 0) {
-                            if (strcmp(tinet_net_buffer, "TCP_CONNECTED\n") == 0) {
-                                // connected to TCP
-                                tcp_connected = true;
-                                return TINET_SUCCESS;
-                            }
-                        }
-                    } while (true);
-                }
-                return TINET_TCP_INIT_FAILED;
-            }
-        }
-
-    } while (1);
-}
-
-srl_device_t tinet_get_srl_device() {
-    return srl_device;
-}
-
-int tinet_write_srl(const char *message) {
-    size_t totalBytesWritten = 0;
-    const size_t messageLength = strlen(message);
-
-    while (totalBytesWritten < messageLength) {
-        const int bytesWritten = srl_Write(&srl_device, message, messageLength);
-
-        if (bytesWritten < 0) {
-            return TINET_SRL_WRITE_FAIL;
-        }
-
-        totalBytesWritten += bytesWritten;
-    }
-
-    usb_HandleEvents();
-    return TINET_SUCCESS;
-}
-
-// TODO: not receiving all the data, fix this
-int tinet_read_srl(char *to_buffer) {
-    int total_bytes_read = 0;
-
-    while (true) {
-        const int bytes_read = srl_Read(&srl_device, to_buffer + total_bytes_read, 128);
-
-        if (bytes_read > 0) {
-            has_srl_device = true;
-            bridge_connected = true;
-            total_bytes_read += bytes_read;
-
-            if (to_buffer[total_bytes_read - 1] == '\n') {
-                break;
-            }
-        } else if (bytes_read < 0) {
-            has_srl_device = false;
-            bridge_connected = false;
-            break;
-        } else {
-            break;
-        }
-    }
-
-    to_buffer[total_bytes_read] = '\0';
-    usb_HandleEvents();
-    return total_bytes_read;
-}
-
 TINET_ReturnCode tinet_login(const int timeout) {
-    if (!bridge_connected || !has_srl_device || !tcp_connected) {
-        return TINET_NO_CONNECTION;
-    }
-
     time_t start_time;
     time_t current_time;
     time(&start_time);
-
-    char calcidStr[sizeof(systemInfo->calcid) * 2 + 1];
-    for (unsigned int i = 0; i < sizeof(systemInfo->calcid); i++)
-    {
-        sprintf(calcidStr + i * 2, "%02X", systemInfo->calcid[i]);
-    }
-
-    char login_msg[93];
-    snprintf(login_msg, sizeof(login_msg), "LOGIN:%s:%s:%s\n", calcidStr, username, authkey);
-
-    const TINET_ReturnCode write_result = tinet_write_srl(login_msg);
-    if (write_result != TINET_SUCCESS) {
-        return TINET_LOGIN_FAILED;
-    }
 
     do {
         time(&current_time);
         if ((int)difftime(current_time, start_time) > timeout) {
             return TINET_TIMEOUT_EXCEEDED;
         }
-        const int read_bytes = tinet_read_srl(tinet_net_buffer);
-        if (read_bytes > 0) {
-            if (strcmp(tinet_net_buffer, "LOGIN_SUCCESS\n") == 0) {
-                return TINET_SUCCESS;
-            } else if (strcmp(tinet_net_buffer, "ALREADY_CONNECTED\n") == 0) {
-                return TINET_LOGIN_ALREADY_CONNECTED;
-            }
-            return TINET_LOGIN_FAILED;
-        }
+        // request a new session token
+        // set sessiontoken to variable
     } while (1);
 
-    authkey[0] = '\0'; // sets the auth key to nothing to prevent memory dump attacks
+    // key shouldn't be used again after a successful session token request
+    calckey[0] = '\0';
     return TINET_SUCCESS;
 }
 
-TINET_ReturnCode tinet_send_rtc_message(const char *recipient, const char *message) {
-    // example end buffer: RTC_CHAT:recipient:message
-    char write_message[117] = "RTC_CHAT:";
-    strcat(write_message, recipient);
-    strcat(write_message, ":");
-    strcat(write_message, message);
-    strcat(write_message, "\n");
-
-    const TINET_ReturnCode rtc_chat_sent = tinet_write_srl(write_message);
-    write_message[0] = '\0';
-
-    switch (rtc_chat_sent) {
-        case TINET_SUCCESS:
-            return TINET_SUCCESS;
-        case TINET_SRL_WRITE_FAIL:
-            return TINET_CHAT_SEND_FAIL;
-        default:
-            return TINET_FAIL;
-    }
+char tinet_get_session_token() {
+    return *sessiontoken;
 }
--- tinet.h
+++ tinet.h
@@ -5,7 +5,7 @@
  * License: Mozilla Public License Version 2.0
  * Description: Allows the user to communicate with the TINET servers
  *--------------------------------------
-*/
+ */
 
 /*
  * Dear programmer,
@@ -19,7 +19,7 @@
  * a warning for the next person:
  *
  * Total hours wasted here = 0 hours.
-*/
+ */
 
 #ifndef TINET_H
 #define TINET_H
@@ -27,92 +27,48 @@
 #include <srldrvce.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
+  /**
+   * @brief Possible return codes for TINET functions.
+   */
+  typedef enum
+  {
+    TINET_SUCCESS,                 /**< General return code for a successful operation. */
+    TINET_FAIL,                    /**< General error code for unknown failures. */
+    TINET_NO_KEYFILE,              /**< No keyfile found. */
+    TINET_INVALID_KEYFILE,         /**< Invalid keyfile format. */
+    TINET_TIMEOUT_EXCEEDED,        /**< Timeout exceeded during execution. */
+    TINET_LOGIN_FAILED,            /**< General error code for a failed login. */
+    TINET_NO_CONNECTION,           /**< General error code for no internet connection */
+  } TINET_ReturnCode;
 
   /**
-  * @brief Indicates whether a serial device is present.
-  */
- extern bool has_srl_device;
+   * @brief Initializes the TINET system.
+   * @return TINET_ReturnCode indicating the initialization result.
+   */
+  int tinet_init();
 
   /**
-  * @brief Indicates whether a connection to TINET bridge is established.
-  */
- extern bool bridge_connected;
+   * @brief Logs in to the TINET account.
+   * @param timeout Timeout (in seconds) for login.
+   * @attention this should only be run after a successful init and connect!
+   */
+  TINET_ReturnCode tinet_login(const int timeout);
 
- /**
- * @brief Indicates whether a connection to TINET TCP server is established.
- */
- extern bool tcp_connected;
+  /* GETTERS */
+  /**
+   * @brief Gets the username associated with the TINET system.
+   * @return A string representing the username.
+   */
+  char *tinet_get_username();
 
- /**
-  * @brief Possible return codes for TINET functions.
-  */
- typedef enum {
-  TINET_SUCCESS,                      /**< General return code for a successful operation. */
-  TINET_FAIL,                         /**< General error code for unknown failures. */
-  TINET_NO_KEYFILE,                   /**< No keyfile found. */
-  TINET_INVALID_KEYFILE,              /**< Invalid keyfile format. */
-  TINET_SRL_INIT_FAIL,                /**< Serial initialization failure. */
-  TINET_SRL_WRITE_FAIL,               /**< Serial write failure. */
-  TINET_SRL_READ_FAIL,                /**< Serial read failure. */
-  TINET_NO_BRIDGE,                    /**< No bridge connected or not initialized. */
-  TINET_TIMEOUT_EXCEEDED,             /**< Timeout exceeded during execution. */
-  TINET_TCP_INIT_FAILED,              /**< Could not create a TCP socket on the bridge. */
-  TINET_UNSUPPORTED_CALC,             /**< Incorrect calc model, only TI-84PCE (Python Edition too) works. */
-  TINET_LOGIN_FAILED,                 /**< General error code for a failed login. */
-  TINET_LOGIN_ALREADY_CONNECTED,      /**< Error code for login if the user already has a session on TINET. */
-  TINET_NO_CONNECTION,                /**< General error code for no connection, might be bridge, srl or something else */
-  TINET_CHAT_SEND_FAIL,               /**< General error code for a failed chat send. */
- } TINET_ReturnCode;
-
- /**
-  * @brief Initializes the TINET system.
-  * @return TINET_ReturnCode indicating the initialization result.
-  */
- int tinet_init();
-
- /**
-  * @brief Gets the username associated with the TINET system.
-  * @return A string representing the username.
-  */
- char* tinet_get_username();
-
- /**
-  * @brief Establishes a connection to the TINET server.
-  * @return TINET_ReturnCode indicating the connection result.
-  */
- int tinet_connect(int timeout);
-
-/**
- * @brief Retrieves the serial device used by TINET.
- * @return The serial device structure.
- */
- srl_device_t tinet_get_srl_device();
-
- /**
-  * @brief Writes a message to the serial device.
-  * @param message The message to be written.
-  * @return TINET_ReturnCode indicating the write result.
-  */
- int tinet_write_srl(const char *message);
-
- /**
-  * @brief Reads data from the serial device.
-  * @param to_buffer The buffer to store the read data.
-  * @return TINET_ReturnCode indicating the read result or the amount of bytes written.
-  */
- int tinet_read_srl(char *to_buffer);
-
- /**
- * @brief Logs in to the TINET account.
- * @param timeout Timeout (in seconds) for login.
- * @attention this should only be run after a successful init and connect!
- */
- TINET_ReturnCode tinet_login(const int timeout);
-
- TINET_ReturnCode tinet_send_rtc_message(const char *recipient, const char *message);
-
+  /**
+   * @brief Get the session token generated for this session.
+   * @attention this should only be run after a successful init and connect!
+   */
+  char tinet_get_session_token();
 #ifdef __cplusplus
 }
 #endif
